d326251a0310cde430a5186a251dbd2fb43be2f6
Lukas Fritzsche <luckifri@yahoo.de>
2016-12-03 12:23:41 +0100

    Added xcode project frame for iBeacon debugging app.

---
+++ BUStedDebug/BUStedDebug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BUStedDebug
+//
+//  Created by Lukas on 03/12/16.
+//  Copyright © 2016 Lukas Fritzsche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BUStedDebug/BUStedDebug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BUStedDebug
+//
+//  Created by Lukas on 03/12/16.
+//  Copyright © 2016 Lukas Fritzsche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
